432f33a4107adf0ea80270b1bcd3aeedd7a09248
Travis Hathaway <travis.j.hathaway@gmail.com>
2023-01-02 10:23:50 +0100

    version bump

--- latz/__version__.py
+++ latz/__version__.py
@@ -1,3 +1,3 @@
 __title__ = "latz"
-__description__ = "Tool for finding images. Maybe with location 🤷‍️..."
-__version__ = "0.1.1"
+__description__ = "Tool for finding images. Maybe with location 🤷..."
+__version__ = "0.1.2"
